92a6c4d536c9cc8769098f337b4cc8aa43989f13
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2023-05-08 08:04:38 -0400

    finishing up ch5

--- code/ch05-recovering-memory/app_refcount.py
+++ code/ch05-recovering-memory/app_refcount.py
@@ -10,18 +10,18 @@ def main():
     v1 = Doomed()
     oid = id(v1)
 
-    # print(f"Step 1: Ref count is {memutil.refs(oid)}")
+    print(f"Step 1: Ref count is {memutil.refs(oid)}")
 
-    # v2 = v1
-    # print(f"Step 2: Ref count is {memutil.refs(oid)}")
+    v2 = v1
+    print(f"Step 2: Ref count is {memutil.refs(oid)}")
 
-    # v2 = None
-    # print(f"Step 3: Ref count is {memutil.refs(oid)}")
+    v2 = None
+    print(f"Step 3: Ref count is {memutil.refs(oid)}")
 
-    # v1 = None
-    # print(f"Step 4: Ref count is {memutil.refs(oid)}")
+    v1 = None
+    print(f"Step 4: Ref count is {memutil.refs(oid)}")
 
-    # print("End of method!")
+    print("End of method!")
 
 
 if __name__ == '__main__':
